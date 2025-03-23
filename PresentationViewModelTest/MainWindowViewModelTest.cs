f95cdfdcd2b325de4c427ed772671a35f672fa6b
RemBar1 <bartczakremigiusz@gmail.com>
2025-03-23 17:18:43 +0100

    fix test

--- PresentationViewModelTest/MainWindowViewModelTest.cs
+++ PresentationViewModelTest/MainWindowViewModelTest.cs
@@ -2,7 +2,6 @@
 using ConcurrentProgramming.Data;
 using ConcurrentProgramming.PresentationModel;
 using ConcurrentProgramming.PresentationViewModel;
-using Moq;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 
@@ -11,31 +10,10 @@ namespace ConcurrentProgramming.PresentationViewModelTests
     [TestClass]
     public class MainWindowViewModelTests
     {
-        private Mock<BallRepository> mockBallRepository;
-        private Mock<BallPhysics> mockBallPhysics;
-        private MainWindowViewModel viewModel;
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            // Przygotowanie mocków
-            mockBallRepository = new Mock<BallRepository>();
-            mockBallPhysics = new Mock<BallPhysics>(mockBallRepository.Object);
-
-            // Inicjalizacja ViewModelu
-            viewModel = new MainWindowViewModel();
-        }
-
         [TestMethod]
-        public void CorrectPropertiesTest()
+        public void MethodNameTest()
         {
-            // Assert, że komenda RestartSimulationCommand jest zainicjalizowana
-            Assert.IsNotNull(viewModel.RestartSimulationCommand);
-            Assert.IsInstanceOfType(viewModel.RestartSimulationCommand, typeof(ICommand));
 
-            // Assert, że kolekcja kul jest poprawnie zainicjalizowana
-            Assert.IsNotNull(viewModel.Balls);
-            Assert.IsInstanceOfType(viewModel.Balls, typeof(ObservableCollection<Ball>));
         }
     }
 }
